3b1e2ebf7f8e57d1af7ee0dd00497d126d1de992
mayooghapramod05 <mayooghapramod05@gmail.com>
2026-04-09 23:57:38 +0530

    Update ReverseInteger.java

--- src/ReverseInteger.java
+++ src/ReverseInteger.java
@@ -1,3 +1,15 @@
-public class ReverseInteger { 
-        // TODO: Read an integer and print its reverse  
+import java.util.Scanner;
+public class HCF {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        System.out.println(a);
+        sc.close();
+    }
 }
